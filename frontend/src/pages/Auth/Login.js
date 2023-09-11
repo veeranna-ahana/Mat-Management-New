dc841787282b963e642ffba8f97e34f29ff80d63
Bedasur Veeranna <bedasur.veeranna@ahana.co.in>
2023-09-11 14:28:57 +0530

    mon110923

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -26,7 +26,7 @@ function Login() {
 
   function submitLogin() {
     postRequest(
-      "http://localhost:5001/user/login",
+      "http://172.16.20.61:5001/user/login",
       { username: username, password: formPassword },
       (data) => {
         if (data.accessToken) {
