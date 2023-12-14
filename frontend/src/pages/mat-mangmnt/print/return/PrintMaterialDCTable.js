49c53278eba71003b231bc07d2cbdd0711af2bc8
ADITYA KUMAR <CodifyWithAditya@Gmail.com>
2023-12-14 18:39:08 +0530

    ui changes in print material issues for return...adi 14dec23

--- frontend/src/pages/mat-mangmnt/print/return/PrintMaterialDCTable.js
+++ frontend/src/pages/mat-mangmnt/print/return/PrintMaterialDCTable.js
@@ -13,10 +13,12 @@ import MLLogo from "../../../../../../frontend/src/ML-LOGO.png";
 //function PrintMaterialDCTable() {
 const styles = StyleSheet.create({
   insideBox: { borderBottom: "1px", padding: "0.6%" },
-  page: {
-    fontSize: 11,
-    flexDirection: "column",
+  fontBold: {
+    //   fontWeight: "bold",
+    fontSize: "10px",
+    fontFamily: "Helvetica-Bold",
   },
+  pageStyling: { padding: "2%", fontSize: "10px", fontFamily: "Helvetica" },
   tableContainer: {
     flexDirection: "row",
     flexWrap: "wrap",
@@ -83,31 +85,31 @@ const styles = StyleSheet.create({
     padding: "5px",
     width: "100%",
     fontWeight: "bold",
-    fontSize: "11px",
+    //fontSize: "11px",
   },
   titleFull1: {
     //border: "1px",
     padding: "5px",
     width: "100%",
-    fontSize: "10px",
+    //fontSize: "10px",
   },
   titleLeft1: {
     //border: "1px",
     padding: "5px",
     width: "40%",
-    fontSize: "10px",
+    //fontSize: "10px",
   },
 
   titleMiddle1: {
     //border: "1px",
     padding: "5px",
     width: "30%",
-    fontSize: "10px",
+    //fontSize: "10px",
   },
   titleMiddle2: {
     padding: "5px",
     // width: "30%",
-    fontSize: "12px",
+    //fontSize: "12px",
   },
   titleRight1: {
     //border: "1px",
@@ -137,6 +139,13 @@ const styles = StyleSheet.create({
 //return <div>PrintMaterialDCTable</div>;
 //}
 
+const copiesNames = [
+  { copyName: "Original for Recipient" },
+  { copyName: "Transporter Copy" },
+  { copyName: "Accounts Copy" },
+  { copyName: "Extra Copy" },
+];
+
 const PrintMaterialDCTable = ({
   formHeader,
   outData,
@@ -145,13 +154,50 @@ const PrintMaterialDCTable = ({
   totalQTYVar,
 }) => (
   <Document>
-    <Page size="A4" style={{ padding: "3%", fontSize: "11" }}>
-      {/* <View>
-      <Text style={{ padding: "1%" }}></Text>
-    </View> */}
-      <View>
-        {/* Top */}
-        <View
+    {copiesNames.map((copyVal, copyKey) => (
+      <>
+        <Page size="A4" style={{ ...styles.pageStyling }}>
+          <View>
+            {/* Top */}
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Image src={MLLogo} style={{ width: "8.3%" }} />
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  //   justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                {/* <Text>{"   "}</Text> */}
+                <Text style={{ ...styles.fontBold }}>
+                  Magod Laser Machining Private Limited
+                </Text>
+                <Text>GST: 29AABCM1970H1ZE CIN: U28900KA1995PTC018437</Text>
+                <Text>
+                  Plot No 72, 2nd Phase, KIADB Indl Area Jigani, Anekal Taluk
+                  Bengaluru - 560105
+                </Text>
+                <Text>
+                  Ph : 08110 414313, 9513393352, sales@magodlaser.in,
+                  www.magodlaser.in
+                </Text>
+                <View>
+                  <Text style={{ ...styles.fontBold }}>
+                    Material / Scarp Sheets Return Challan
+                  </Text>
+                </View>
+              </View>
+              <Text style={{ width: "10%" }}>{copyVal.copyName}</Text>
+            </View>
+            {/* <View
           style={{
             display: "flex",
             flexDirection: "row",
@@ -180,157 +226,184 @@ const PrintMaterialDCTable = ({
               +91-80-42291005, +91-8110-414313, info@magodlaser.in,
               https://www.magodlaser.in/
             </Text>
-            {/* <Text>Material Stock List as On : {formatDate(new Date(), 7)}</Text> */}
           </View>
           <Text style={{ padding: "3%" }}></Text>
-        </View>
-        <Text style={{ padding: "1%" }}></Text>
-        <View style={{ border: "1px" }}>
-          <View style={{ display: "flex", flexDirection: "row" }}>
-            <View
-              style={{
-                ...styles.insideBox,
-                width: "70%",
-                // borderBottom: "1px",
-                borderRight: "1px",
-              }}
-            >
-              <Text style={{ fontWeight: "bold" }}>
-                {formHeader?.Customer} ({formHeader?.Cust_code})
-              </Text>
-              <View style={{ padding: "0.6%" }}>
-                <Text>GST No: {custdata?.GSTNo}</Text>
-                <Text>Branch: {custdata?.Branch}</Text>
-                {/* <View style={{ display: "flex", flexDirection: "row" }}> */}
-                <Text>{custdata?.Address}</Text>
-                <Text>
-                  {custdata?.City}, {custdata?.State} - {custdata?.Pin_Code}
-                </Text>
-                {/* <Text>
+        </View> */}
+            <Text style={{ padding: "0.5%" }}></Text>
+            <View style={{ border: "1px" }}>
+              <View style={{ display: "flex", flexDirection: "row" }}>
+                <View
+                  style={{
+                    ...styles.insideBox,
+                    width: "70%",
+                    // borderBottom: "1px",
+                    borderRight: "1px",
+                  }}
+                >
+                  <Text style={{ ...styles.fontBold }}>
+                    {formHeader?.Customer} ({formHeader?.Cust_code})
+                  </Text>
+                  <View style={{ padding: "0.6%" }}>
+                    <View style={{ display: "flex", flexDirection: "row" }}>
+                      <Text style={{ ...styles.fontBold }}>GSTIN : </Text>
+                      <Text>{custdata?.GSTNo}</Text>
+                    </View>
+
+                    <View style={{ display: "flex", flexDirection: "row" }}>
+                      <Text style={{ ...styles.fontBold }}>Branch:</Text>
+
+                      <Text>{custdata?.Branch}</Text>
+                    </View>
+
+                    {/* <View style={{ display: "flex", flexDirection: "row" }}> */}
+                    <Text>{custdata?.Address}</Text>
+                    <Text>
+                      {custdata?.City}, {custdata?.State} - {custdata?.Pin_Code}
+                    </Text>
+                    {/* <Text>
                   {custdata?.City} 
                 </Text> */}
-                {/* <Text></Text> */}
-                {/* </View> */}
-              </View>
-            </View>
-
-            <View
-              style={{
-                ...styles.insideBox,
-                width: "30%",
-                display: "flex",
-                flexDirection: "column",
-                // justifyContent: "center",
-                // borderBottom: "1px",
-                // borderRight: "1px",
-              }}
-            >
-              <Text>DC No : {formHeader?.PkngDcNo}</Text>
-              <Text>IV No : {formHeader?.IV_No}</Text>
-              <Text>
-                IV Date : {formHeader?.IV_Date}
-                {/* {formatDate(
-                  new Date(
-                    new Date(
-                      formHeader?.IV_Date.toString().substring(0, 10)
-                    ).toDateString()
-                  ),
-                  1
-                )} */}
-              </Text>
-            </View>
-          </View>
-
-          {/* <View style={{ ...styles.titleRight1 }}></View> */}
-
-          {/* <Text style={styles.titleFull1}> */}
-          <View style={{ ...styles.insideBox }}>
-            <Text>Authority : {dcRegister?.AuhtorisingDocu}</Text>
-          </View>
-
-          {/* <Text style={styles.topspace}></Text> */}
-
-          <View
-            style={{
-              ...styles.insideBox,
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "flex-start",
-            }}
-          >
-            <Text style={styles.tableCol1}>SL No</Text>
-            <Text style={styles.tableCol2}>Description</Text>
-            <Text style={styles.tableCol3}>Material</Text>
-            <Text style={styles.tableCol4}>Quantity</Text>
-          </View>
-          <View
-            style={{
-              ...styles.insideBox,
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            {outData?.map((item, index) => {
-              return (
-                <>
-                  <View
-                    style={{
-                      display: "flex",
-                      flexDirection: "row",
-                      justifyContent: "flex-start",
-                    }}
-                  >
-                    <Text style={styles.tableCol1}>{index + 1}</Text>
-                    <Text style={styles.tableCol2}>
-                      {
-                        item.MtrlDescription?.split("/**")[0]
-                          ?.split(".00")[0]
-                          ?.replace(/[0-9]/g, "")
-                          ?.split("Quantity")[0]
-                      }
-                    </Text>
-                    <Text style={styles.tableCol3}>{item.Material}</Text>
-                    <Text style={styles.tableCol4}>{item.Qty}</Text>
+                    {/* <Text></Text> */}
+                    {/* </View> */}
                   </View>
-                </>
-              );
-            })}
-          </View>
+                </View>
 
-          {/* <Text style={styles.topspace}></Text>
+                <View
+                  style={{
+                    ...styles.insideBox,
+                    width: "30%",
+                    display: "flex",
+                    flexDirection: "column",
+                    // justifyContent: "center",
+                    // borderBottom: "1px",
+                    // borderRight: "1px",
+                  }}
+                >
+                  <View style={{ display: "flex", flexDirection: "row" }}>
+                    <Text style={{ ...styles.fontBold }}>DC No : </Text>
+                    <Text>{formHeader?.PkngDcNo}</Text>
+                  </View>{" "}
+                  <View style={{ display: "flex", flexDirection: "row" }}>
+                    <Text style={{ ...styles.fontBold }}>IV No : </Text>
+                    <Text>{formHeader?.IV_No}</Text>
+                  </View>
+                  <View style={{ display: "flex", flexDirection: "row" }}>
+                    <Text style={{ ...styles.fontBold }}>IV Date : </Text>
+                    <Text>{formHeader?.IV_Date}</Text>
+                  </View>
+                </View>
+              </View>
+
+              {/* <View style={{ ...styles.titleRight1 }}></View> */}
+
+              {/* <Text style={styles.titleFull1}> */}
+              <View style={{ ...styles.insideBox }}>
+                <View style={{ display: "flex", flexDirection: "row" }}>
+                  <Text style={{ ...styles.fontBold }}>Authority : </Text>
+                  <Text>{dcRegister?.AuhtorisingDocu}</Text>
+                </View>
+              </View>
+
+              {/* <Text style={styles.topspace}></Text> */}
+
+              <View
+                style={{
+                  ...styles.insideBox,
+                  ...styles.fontBold,
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "flex-start",
+                }}
+              >
+                <Text style={styles.tableCol1}>SL No</Text>
+                <Text style={styles.tableCol2}>Description</Text>
+                <Text style={styles.tableCol3}>Material</Text>
+                <Text style={styles.tableCol4}>Quantity</Text>
+              </View>
+              <View
+                style={{
+                  ...styles.insideBox,
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                {outData?.map((item, index) => {
+                  return (
+                    <>
+                      <View
+                        style={{
+                          display: "flex",
+                          flexDirection: "row",
+                          justifyContent: "flex-start",
+                        }}
+                      >
+                        <Text style={styles.tableCol1}>{index + 1}</Text>
+                        <Text style={styles.tableCol2}>
+                          {
+                            item.MtrlDescription?.split("/**")[0]
+                              ?.split(".00")[0]
+                              ?.replace(/[0-9]/g, "")
+                              ?.split("Quantity")[0]
+                          }
+                        </Text>
+                        <Text style={styles.tableCol3}>{item.Material}</Text>
+                        <Text style={styles.tableCol4}>{item.Qty}</Text>
+                      </View>
+                    </>
+                  );
+                })}
+              </View>
+
+              {/* <Text style={styles.topspace}></Text>
           <Text style={styles.topspace}></Text>
           <Text style={styles.topspace}></Text>
           <Text style={styles.topspace}></Text> */}
-          {/* <Text style={styles.titleLeft1}></Text> */}
-          <View style={{ padding: "1%" }}></View>
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              padding: "0.6%",
-            }}
-          >
-            <Text>Remarks :</Text>
-            <View style={{ padding: "5%", border: "1px" }}></View>
+              {/* <Text style={styles.titleLeft1}></Text> */}
+              <View style={{ padding: "1%" }}></View>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  padding: "0.6%",
+                }}
+              >
+                <Text style={{ ...styles.fontBold }}>Remarks :</Text>
+                <View style={{ padding: "5%", border: "1px" }}></View>
+              </View>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-around",
+                }}
+              >
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    ...styles.titleMiddle2,
+                  }}
+                >
+                  <Text style={{ ...styles.fontBold }}>Total Items : </Text>
+                  <Text>{outData?.length}</Text>
+                </View>
+
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    ...styles.titleMiddle2,
+                  }}
+                >
+                  <Text style={{ ...styles.fontBold }}>Total Quantity : </Text>
+                  <Text>{totalQTYVar}</Text>
+                </View>
+              </View>
+            </View>
           </View>
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-around",
-            }}
-          >
-            <Text style={styles.titleMiddle2}>
-              Total Items : {outData?.length}
-            </Text>
-            {/* <Text style={styles.titleLeft1}></Text> */}
-            <Text style={styles.titleMiddle2}>
-              Total Quantity : {totalQTYVar}
-            </Text>
-          </View>
-        </View>
-      </View>
-    </Page>
+        </Page>
+      </>
+    ))}
   </Document>
 );
 
