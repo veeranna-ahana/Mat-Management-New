cb7b53a8436ae267e8f2b97435b85fe014e090e2
Kanaka C S <kanaka.cs@ahanait.com>
2024-01-11 16:47:32 +0530

    PDF Changes in ShopFloor

--- frontend/src/pages/mat-mangmnt/print/shopfloorissue/PrintIVListProfileCuttingTable1.js
+++ frontend/src/pages/mat-mangmnt/print/shopfloorissue/PrintIVListProfileCuttingTable1.js
@@ -282,13 +282,13 @@ const PrintIVListProfileCuttingTable1 = ({ formHeader, tableData }) => (
 
         <View style={styles.blockRightAlign2}>
           <Text>Material</Text>
-          <Text style={styles.linegap}>Length</Text>
+          <Text style={styles.linegap}>Height</Text>
           <Text style={styles.linegap}>Qty</Text>
           <Text style={styles.linegap}>Source</Text>
         </View>
         <View style={styles.blockLeftAlign2}>
           <Text>{formHeader.Mtrl_Code}</Text>
-          <Text style={styles.linegap}>{formHeader.Para1}</Text>
+          <Text style={styles.linegap}>{formHeader.Para3}</Text>
           <Text style={styles.linegap}>{formHeader.Qty}</Text>
           <Text style={styles.linegap}>Custom</Text>
         </View>
@@ -303,11 +303,11 @@ const PrintIVListProfileCuttingTable1 = ({ formHeader, tableData }) => (
         </View>
 
         <View style={styles.blockRightAlign2}>
-          <Text>Height</Text>
+          <Text>Length</Text>
           <Text style={styles.linegap}>Process</Text>
         </View>
         <View style={styles.blockLeftAlign2}>
-          <Text>{formHeader.Para3}</Text>
+          <Text>{formHeader.Para1}</Text>
           <Text style={styles.linegap}>{formHeader.MProcess}</Text>
         </View>
 
